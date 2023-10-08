b288c4a8aebc2dad5fbb259a2a06af6b2964ef94
Dorian Walton <80310629+dwalto35@users.noreply.github.com>
2023-10-08 00:40:25 -0400

    Update parallel.c

--- parallel.c
+++ parallel.c
@@ -15,7 +15,7 @@ int totalNum = 0;
 
 int readf(FILE *fp) {
     if ((fp = fopen("emoji.txt", "r")) == NULL) {
-        printf("ERROR: can't open string.txt!\n");
+        printf("ERROR: can't open emoji.txt!\n");
         return 0;
     }
     s1 = (char *)malloc(sizeof(char) * MAX);
@@ -28,7 +28,7 @@ int readf(FILE *fp) {
         printf("ERROR: Out of memory\n");
         return -1;
     }
-    /* read s1 s2 from the file */
+    /* read s1 and s2 from the file */
     s1 = fgets(s1, MAX, fp);
     s2 = fgets(s2, MAX, fp);
     n1 = strlen(s1); /* length of s1 */
@@ -44,7 +44,7 @@ int num_substring(int t) {
     int start = t * (n1 / NUM_THREADS);
     int end = (t == NUM_THREADS - 1) ? n1 : (t + 1) * (n1 / NUM_THREADS);
 
-    for (int i = start; i <= n1 - n2; i++) {
+    for (int i = start; i <= end - n2; i++) {
         int j;
         for (j = 0; j < n2; j++) {
             if (s1[i + j] != s2[j]) {
