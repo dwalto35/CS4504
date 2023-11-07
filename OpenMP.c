c447273600a5ad27ec462b1cbd4fffa66a270ab8
Dorian Walton <80310629+dwalto35@users.noreply.github.com>
2023-11-06 20:30:03 -0500

    Update OpenMP.c

--- OpenMP.c
+++ OpenMP.c
@@ -12,51 +12,9 @@ double firstMatrix[N3][N3] = {0.0};
 double secondMatrix[N3][N3] = {0.0};
 double matrixMultiResult[N3][N3] = {0.0};
 
-void matrixMulti(int n, clock_t *serialTime, clock_t *parallelTime)
+void matrixMulti(int n)
 {
-    // Serial execution
-    clock_t t1, t2;
-    
-    // Reset the result matrix
-    memset(matrixMultiResult, 0, sizeof(matrixMultiResult));
-    
-    matrixInit(n);
-    t1 = clock();
-    matrixMultiSerial(n);
-    t2 = clock();
-    *serialTime = t2 - t1;
-
-    // Parallel execution
-    t1 = clock();
-    #pragma omp parallel
-    {
-        #pragma omp single
-        matrixMultiParallel(n);
-    }
-    t2 = clock();
-    *parallelTime = t2 - t1;
-}
-
-void matrixMultiSerial(int n)
-{
-    for (int row = 0; row < n; row++)
-    {
-        for (int col = 0; col < n; col++)
-        {
-            double resultValue = 0;
-
-            for (int transNumber = 0; transNumber < n; transNumber++)
-            {
-                resultValue += firstMatrix[row][transNumber] * secondMatrix[transNumber][col];
-            }
-            matrixMultiResult[row][col] = resultValue;
-        }
-    }
-}
-
-void matrixMultiParallel(int n)
-{
-    #pragma omp for
+    #pragma omp parallel for
     for (int row = 0; row < n; row++)
     {
         for (int col = 0; col < n; col++)
@@ -88,19 +46,56 @@ void matrixInit(int n)
 
 int main()
 {
-    clock_t serialTime, parallelTime;
+    clock_t t1, t2;
+    double t1_omp, t2_omp;
     
     // Case N = 1024
-    matrixMulti(N1, &serialTime, &parallelTime);
-    printf("N = 1024, Serial Time: %ld, Parallel Time: %ld\n", serialTime, parallelTime);
+    matrixInit(N1);
+    
+    // Serial execution
+    t1 = clock();
+    matrixMulti(N1);
+    t2 = clock();
+    printf("N = 1024, Sequential Time: %ld\n", t2 - t1);
+    
+    // Parallel execution
+    matrixInit(N1);
+    t1_omp = omp_get_wtime();
+    matrixMulti(N1);
+    t2_omp = omp_get_wtime();
+    printf("N = 1024, Parallel Time: %f\n", t2_omp - t1_omp);
     
     // Case N = 2048
-    matrixMulti(N2, &serialTime, &parallelTime);
-    printf("N = 2048, Serial Time: %ld, Parallel Time: %ld\n", serialTime, parallelTime);
-
+    matrixInit(N2);
+    
+    // Serial execution
+    t1 = clock();
+    matrixMulti(N2);
+    t2 = clock();
+    printf("N = 2048, Sequential Time: %ld\n", t2 - t1);
+    
+    // Parallel execution
+    matrixInit(N2);
+    t1_omp = omp_get_wtime();
+    matrixMulti(N2);
+    t2_omp = omp_get_wtime();
+    printf("N = 2048, Parallel Time: %f\n", t2_omp - t1_omp);
+    
     // Case N = 4096
-    matrixMulti(N3, &serialTime, &parallelTime);
-    printf("N = 4096, Serial Time: %ld, Parallel Time: %ld\n", serialTime, parallelTime);
+    matrixInit(N3);
+    
+    // Serial execution
+    t1 = clock();
+    matrixMulti(N3);
+    t2 = clock();
+    printf("N = 4096, Sequential Time: %ld\n", t2 - t1);
+    
+    // Parallel execution
+    matrixInit(N3);
+    t1_omp = omp_get_wtime();
+    matrixMulti(N3);
+    t2_omp = omp_get_wtime();
+    printf("N = 4096, Parallel Time: %f\n", t2_omp - t1_omp);
 
     return 0;
 }
